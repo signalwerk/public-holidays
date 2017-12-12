6241a6798ab3868ef98869a6f04917317288b606
signalwerk <sh@signalwerk.ch>
2017-12-12 02:01:04 +0100

    ADD: initial script

---
+++ src/easterDate.js
@@ -0,0 +1,40 @@
+// Takes a given year (y) then returns Date object of Easter Sunday
+export function easterDate(y) {
+	/*
+		Easter Date Function for JavaScript implemented by Furgelnod ( https://furgelnod.com )
+		Using algorithm published at The Date of Easter (on aa.usno.navy.mil, Oct 2007)
+		(https://web.archive.org/web/20071015045929/http://aa.usno.navy.mil/faq/docs/easter.php)
+		The algorithm is credited to J.-M. Oudin (1940) and is reprinted in the
+		Explanatory Supplement to the Astronomical Almanac, ed. P. K. Seidelmann (1992).
+		See Chapter 12, "Calendars", by L. E. Doggett.
+	*/
+	try {
+		y = Number( y );
+		if ( y != y ) {
+			throw new TypeError( "Value must be a number." );
+		}
+		else if ( y > 275760 || y < -271820 ) {
+			throw new RangeError( "Value be between -271820 and 275760 due to technical limitations of Date constructor." );
+		}
+	}
+	catch ( e ) { console.error( e ); }
+
+	y = Math.floor( y );
+	var c = Math.floor( y / 100 );
+	var n = y - 19 * Math.floor( y / 19 );
+	var k = Math.floor( ( c - 17 ) / 25 );
+	var i = c - Math.floor( c / 4 ) - Math.floor( ( c - k ) / 3 ) + 19 * n + 15;
+	i = i - 30 * Math.floor( i / 30 );
+	i = i - Math.floor( i / 28 ) * ( 1 - Math.floor( i / 28 ) * Math.floor( 29 / ( i + 1 ) ) * Math.floor( ( 21 - n ) / 11 ) );
+	var j = y + Math.floor( y / 4 ) + i + 2 - c + Math.floor( c / 4 );
+	j = j - 7 * Math.floor( j / 7 );
+	var l = i - j;
+	var m = 3 + Math.floor( ( l + 40 ) / 44 );
+	var d = l + 28 - 31 * Math.floor( m / 4 );
+
+  return {
+    year: y,
+    month: m,
+    day: d,
+  }
+} // -- easterDate
---
+++ src/index.js
@@ -0,0 +1,165 @@
+import fs from 'fs';
+import yaml from 'js-yaml';
+import moment from 'moment';
+import uuid from 'uuid';
+import { easterDate } from './easterDate'
+
+import {
+  Component,
+  Property
+} from 'immutable-ics'
+
+
+
+
+const now = new moment();
+
+
+
+
+class PublicHoliday {
+
+  constructor(year) {
+    this.year = year || now.year();
+    this.events = [];
+  }
+
+  // get yml data
+  loader(path) {
+    try {
+      return yaml.safeLoad(fs.readFileSync(path, 'utf8'));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  // add events based on a yaml
+  add(path, lang) {
+    let data = this.loader(path)
+    this.process(data, lang)
+  }
+
+
+  // process each week
+  process(data, lang) {
+
+    let date;
+
+    switch(data.start.type) {
+      case 'date':
+        date = moment.utc({ year: this.year, month: data.start.month - 1, day: data.start.day})
+        break;
+      case 'easter':
+        // code block
+        let easter = easterDate(this.year);
+
+        date = moment.utc({
+          year: easter.year,
+          month: easter.month - 1,
+          day: easter.day
+        })
+
+        break;
+      default:
+        throw "wrong type: " + data.start.type;
+    }
+
+    if(data.math) {
+      date.add(data.math.months || 0, 'months');
+      date.add(data.math.weeks || 0, 'weeks');
+      date.add(data.math.days || 0, 'days');
+    }
+
+    this.events.push({
+      title: data.name[lang],
+      date,
+    })
+  }
+
+  // generate the icsEvent
+  icsEvent(data) {
+
+    var properties = [
+      new Property({
+        name: 'UID',
+        value: uuid.v1()
+      }),
+      new Property({
+        name: 'DTSTAMP',
+        value: now.toDate(),
+        parameters: {
+          // VALUE: 'DATE-TIME',
+          // TZID: 'Europe/Zurich',
+        }
+      }),
+      new Property({
+        name: 'SUMMARY',
+        value: data.title,
+      }),
+      new Property({
+        name: 'DTSTART',
+        value: data.date.toDate(),
+        parameters: {
+          VALUE: 'DATE',
+          // TZID: 'Europe/Zurich',
+        }
+      }),
+      new Property({
+        name: 'DTEND',
+        value: data.date.add(1, 'd').toDate(),
+        parameters: {
+          VALUE: 'DATE',
+          // TZID: 'Europe/Zurich',
+        }
+      }),
+    ]
+
+
+    var event = new Component({
+      name: 'VEVENT',
+      properties
+    });
+
+    return event;
+  }
+
+  // generate the ics
+  ics(path) {
+    var events = [];
+
+    this.events.forEach((value) => {
+      events.push(this.icsEvent(value))
+    });
+
+    const calendar = new Component({
+      name: 'VCALENDAR',
+      components: events,
+      properties: [
+        new Property({
+          name: 'VERSION',
+          value: 2
+        }),
+        new Property({
+          name: 'PRODID',
+          value: 'PublicHoliday'
+        })
+      ]
+    })
+
+    let out = calendar.toString();
+    fs.writeFileSync(path, out);
+    console.log('ics written to ' + path);
+  }
+}
+
+
+
+console.log('-- start ics generation --');
+let ics = new PublicHoliday(2018);
+ics.add('./src/rules/NewYearsDay.yaml', "DE-DE");
+ics.add('./src/rules/easterRelative/GoodFriday.yaml', "DE-DE");
+
+// ics.add('./src/data/IAD2017/semester2017HS.yaml');
+// ics.add('./src/data/IAD2017/semester2018FS.yaml');
+ics.ics('./IAD.ics');
+console.log('-- end ics generation --');
